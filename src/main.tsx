7ea2a3279d2dbd9cf6017c69a99e7a5e1b355a46
SingularityT3 <44658109+SingularityT3@users.noreply.github.com>
2025-11-13 09:21:21 +0530

    Revert "create auth context and login, signup page"
    This reverts commit 0dbb61f1227f360e871891c9df46b8228c1bd11e.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import LandingPage from "./pages/landing/landing.tsx";
+import { LoginPage, SignupPage } from "./pages/auth/auth.tsx";
+import HomePage from "./pages/home/home.tsx";
 import "./index.css";
-import App from "./App";
-import { AuthProvider } from "./context/User";
 
 createRoot(document.getElementById("root")!).render(
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/home" element={<HomePage />} />
+    </Routes>
+  </BrowserRouter>
 );
