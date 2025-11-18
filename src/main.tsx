1593244ddc591866cd1c9353c8b6ed9e6ef8837b
SingularityT3 <44658109+SingularityT3@users.noreply.github.com>
2025-11-18 09:53:43 +0530

    Revert "Merge branch 'main' into ui-revamp"
    This reverts commit 7f264c721ea6f348530fbbc3df76b45bb41f8211, reversing
    changes made to cc623db45e2346a636ac69490c014e739f0350e4.

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
-import LandingPage from "./pages/landing/landing.tsx";
-import { LoginPage, SignupPage } from "./pages/auth/auth.tsx";
-import HomePage from "./pages/home/home.tsx";
 import "./index.css";
+import App from "./App";
+import { AuthProvider } from "./context/User";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/home" element={<HomePage />} />
-    </Routes>
-  </BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
 );
